b90e86d88b6114480642ae9431d3ff04a89c2e43
Bensalah <amine.bensalah@outlook.com>
2018-03-12 12:56:13 +0100

    initial cross platform sdk

---
+++ Sources/Debuggable.h
@@ -0,0 +1,18 @@
+//
+//  Debuggable.h
+//  Debuggable
+//
+//  Created by Amine Bensalah on 12/03/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Debuggable.
+FOUNDATION_EXPORT double DebuggableVersionNumber;
+
+//! Project version string for Debuggable_iOS.
+FOUNDATION_EXPORT const unsigned char DebuggableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Debuggable/PublicHeader.h>
+
+
